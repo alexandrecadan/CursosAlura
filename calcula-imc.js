9722c698f8d1b435ec5b4c9d292f333525ea23ee
alexandre <alexandrecadan@gmail.com>
2017-02-12 12:38:56 -0200

    Subindo os arquivos atualizados

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,26 +1,39 @@
 
-var trsPacientes = document.getElementsByClassName("paciente");
+function CalculaImc() {
+  var trsPacientes = document.getElementsByClassName("paciente");
 
-var posicaoAtual = 0;
+  var posicaoAtual = 0;
 
-for(var posicaoAtual; posicaoAtual < trsPacientes.length; posicaoAtual++) {
+  for (var posicaoAtual; posicaoAtual < trsPacientes.length; posicaoAtual++) {
 
-  var pacienteTr = trsPacientes[posicaoAtual];
+    var pacienteTr = trsPacientes[posicaoAtual];
 
-  var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
-  var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
-  var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
-  var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
 
-  var pacientes = {nome : tdNome.textContent, peso : tdPeso.textContent, altura : tdAltura.textContent };
+    var pacientes = { nome: tdNome.textContent, peso: tdPeso.textContent, altura: tdAltura.textContent };
 
-  if (pacientes.altura != 0) {
+    if (pacientes.altura != 0) {
 
-    var imc = pacientes.peso / (pacientes.altura * pacientes.altura);  
+      var imc = pacientes.peso / (pacientes.altura * pacientes.altura);
 
-    tdImc.textContent = imc;
-    
-  } else {
-    console.log("Não executei porque altura é igual a zero");
+      tdImc.textContent = imc;
+
+    } else {
+      console.log("Não executei porque altura é igual a zero");
+    }
   }
+}
+
+function AdicionarPaciente(){
+
+  var Nome = document.getElementById("campo-nome");
+  var Peso = document.getElementById("campo-peso");
+  var Altura = document.getElementById("campo-altura");
+
+  
+
+
 }
